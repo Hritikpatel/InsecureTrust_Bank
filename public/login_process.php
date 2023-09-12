1ad2895d441c1b5bae52fdab1e121829e84375a1
Hritikpatel <83468913+Hritikpatel@users.noreply.github.com>
2023-09-12 23:45:52 +0530

    .

--- public/login_process.php
+++ public/login_process.php
@@ -4,14 +4,31 @@ session_start();
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Replace these with your actual authentication logic
-    $valid_username = "user123";
-    $valid_password = "password123";
 
     $entered_username = $_POST["username"];
     $entered_password = $_POST["password"];
 
-    // Check if the entered credentials are valid
-    if ($entered_username === $valid_username && $entered_password === $valid_password) {
+    // Database connection (replace with your database details)
+    $server = "localhost";
+    $username = "root"; // Default username for XAMPP MySQL
+    $password = "";     // Default password for XAMPP MySQL (empty by default)
+    $database = "your_database_name"; // Replace with your database name
+
+    // Create a connection
+    $conn = new mysqli($server, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare a SQL statement
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $entered_username, $entered_password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
         // Authentication successful
         $_SESSION["authenticated"] = true;
         header("Location: dashboard.php"); 
@@ -22,6 +39,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: login.php"); 
         exit();
     }
+
+    $stmt->close();
+    $conn->close();
 } else {
     // Handle unauthorized access or other HTTP methods
     header("HTTP/1.1 401 Unauthorized");
