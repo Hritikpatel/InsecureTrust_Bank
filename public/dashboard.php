f9acce90bbe43245475ac9999c0bdbf072f16671
Hritikpatel <83468913+Hritikpatel@users.noreply.github.com>
2023-09-13 11:11:01 +0530

    Created Dashboard #1

---
+++ public/dashboard.php
@@ -0,0 +1,15 @@
+<!-- dashboard.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../assets/css/dash.css">
+</head>
+<body>
+    <div class="container">
+        <h1>dashboard</h1>
+    </div>
+</body>
+</html>
