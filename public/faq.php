6e40707a2c6363e0a0f79a67619f457161f85e12
Hritikpatel <83468913+Hritikpatel@users.noreply.github.com>
2023-09-17 11:54:39 +0530

    .

---
+++ public/faq.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITB-FAQ</title>
+    <link rel="stylesheet" href="../assets/css/faq.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="body">
+        <nav class="navbar">
+            <div class="logo">
+                ITB
+            </div>
+            <ul class="list">
+                <li class="item">Home</li>
+                <li class="item">About Us</li>
+                <li class="item">Discover</li>
+                <li class="item">Support</li>
+                <li class="item">FAQs</li>
+            </ul>
+            <a href="login.php"><button type="button">Log In</button></a>
+        </nav>
+    </div>
+</body>
+</html>
