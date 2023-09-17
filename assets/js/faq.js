4d2cd85e3e505f3bc4005f80db6150ebfd90a4b0
Hritikpatel <83468913+Hritikpatel@users.noreply.github.com>
2023-09-17 20:29:04 +0530

    .

---
+++ assets/js/faq.js
@@ -0,0 +1,107 @@
+
+const FAQ = [
+  {
+    question: "How many team members can I invite ?",
+    answer:
+      `To open a bank account with us, follow these steps:
+
+      <br>1. Visit our nearest branch or apply online through our website.
+      <br>2. Choose the type of account that suits your needs (e.g., savings, checking, or a specific product).
+      <br>3. Provide the required identification documents, such as a valid government-issued ID, proof of address, and Social Security Number.
+      <br>4. Complete the application form with accurate personal information.
+      <br>5. Make an initial deposit as per the account's minimum balance requirement.
+      <br>6. Review and accept the terms and conditions.
+      <br>7. Once your application is approved, you'll receive your account details and can start using your new account.`
+  },
+  {
+    question: "What is the maximum file upload size ?",
+    answer:
+      "No more than 2GB. All files in your account must fit your allotted storage space."
+  },
+  {
+    question: "How do I reset my password ?",
+    answer:
+      "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you"
+  },
+  {
+    question: "Can I cancel my subscription ?",
+    answer:
+      "Yes! Send us a message and we’ll process your request no questions asked."
+  },
+  {
+    question: "Do you provide additional support ?",
+    answer:
+      "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+  }
+];
+
+const mainContainerEl = document.getElementById("mainContainer");
+
+function FAQuestions() {
+  return FAQ.forEach((item) => {
+    return createHTMLElements(item.question, item.answer);
+  });
+}
+
+function createHTMLElements(question, answer) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const questionCont = document.createElement("div");
+  questionCont.classList.add("question");
+  card.appendChild(questionCont);
+  const questionPara = document.createElement("div");
+  questionPara.classList.add("questionpara");
+  const questionEl = document.createElement("p");
+  questionCont.appendChild(questionPara);
+  questionPara.appendChild(questionEl);
+  questionEl.innerHTML = question;
+  const arrowCont = document.createElement("div");
+  const arrowImg = document.createElement("img");
+  arrowImg.src =
+    "https://raw.githubusercontent.com/Dinesh1042/FAQ-Cards/f691e8334fd253cfe843ab983e070294cd880b10/images/icon-arrow-down.svg";
+  arrowCont.classList.add("arrowCont");
+  arrowCont.appendChild(arrowImg);
+  questionCont.appendChild(arrowCont);
+
+  // Answer Element
+  const answerCont = document.createElement("div");
+  answerCont.classList.add("answer");
+  const answerPara = document.createElement("div");
+  answerPara.classList.add("answerPara");
+  answerCont.appendChild(answerPara);
+  const answerEl = document.createElement("p");
+  answerEl.innerHTML = answer;
+  answerPara.appendChild(answerEl);
+  card.appendChild(answerCont);
+  mainContainerEl.appendChild(card);
+
+  // All Cont
+}
+
+FAQuestions();
+
+const allCont = mainContainerEl.querySelectorAll(".card");
+
+allCont.forEach((e) => {
+  const btn = e.querySelector(".question");
+
+  btn.addEventListener("click", (pos) => {
+    allCont.forEach((item) => {
+      if (item !== e) {
+        item.classList.remove("active");
+      }
+    });
+    e.classList.toggle("active");
+  });
+});
+
+
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get form data
+  var formData = document.getElementById("searchbar").text;
+  console.log(formData);
+  alert(formData);
+  // document.getElementById("searchTerm").textContent= 
+});
--- public/faq.php
+++ public/faq.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="../assets/js/faq.js" defer></script>
 </head>
 <body>
     <nav class="navbar">
@@ -24,7 +26,26 @@
         <a href="login.php" ><button type="button" class="btn">Login</button></a>
     </nav>
     <div class="body">
-        
+        <section class="top">
+            <span class="sub">FAQ</span>
+            <span class="heading">Have any questions?</span>
+            <form class="searchBarForm" id="form">
+                <input type="text" name="search" id="searchbar" placeholder="Search your question..." required>
+                <button type="submit" id="searchBtn"><i class="fa-solid fa-magnifying-glass fa-xl" style="color: #191919;"></i></button>
+            </form>
+            <span id="searchTerm"></span>
+        </section>
+        <ul class="categories">
+            <li class="tags activeTag" id="account">Account</li>
+            <li class="tags" id="banking">Online Banking</li>
+            <li class="tags" id="trans">Transactions</li>
+            <li class="tags" id="credit">Credit and Loans</li>
+            <li class="tags" id="other">Others</li>
+        </ul>
+        <section class="faqs" id="faqs">
+            <div class="questionCont" id="mainContainer">
+            </div>
+        </section>
     </div>
 </body>
 </html>
