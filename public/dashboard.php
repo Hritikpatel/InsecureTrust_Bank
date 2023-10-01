de890a6c98d6fe98560e9b41f0452299083fda1e
Hritikpatel <83468913+Hritikpatel@users.noreply.github.com>
2023-10-01 17:44:06 +0530

    .

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,5 +1,4 @@
 <!-- dashboard.php -->
-
 <?php
     session_start();
     if (!$_SESSION["authenticated"]) {
@@ -17,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ITB-Dash</title>
     <link rel="stylesheet" href="../assets/css/dash.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="../assets/js/dash.js"></script>
 </head>
@@ -33,17 +33,99 @@
             <li class="item"><a href="faq.php">FAQs</a></li>
         </ul>
         <a href="login.php" ><button type="button" class="btn">Logout</button></a>
-        
     </nav>
     <div class="body">
+        <div class="info"></div>
         <div class="main">
-            <div class="saving"></div>
+            <div class="saving">
+
+            </div>
             <div class="record"></div>
             <div class="extra"></div>
         </div>
         <div class="side">
-            <div class="transactions"></div>
-            <div class="card"></div>
+            <div class="transactions">
+                <div class="header">
+                    <div class="head">Transactions</div>
+                    <div class="listing">
+                        Monthly
+                    </div>
+                </div>
+                <div class="trans-body">
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                    <div class="trans-record">
+                        <div class="icon">
+                            <img src="../assets/images/deposit-s.png" alt="" srcset="">
+                        </div>
+                        <div class="data">
+                            <div class="name">Hritik</div>
+                            <div class="date">09:19:29 12/02/2023</div>
+                        </div>
+                        <div class="amt">$5,000</div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+            </div>
         </div>
     </div>
 </body>
